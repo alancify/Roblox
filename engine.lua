642ecbb930797a5a354d2d75dacc00b336d206fa
alancify <127683771+alancify@users.noreply.github.com>
2023-03-23 12:14:36 +0500

    Create engine.lua

---
+++ engine.lua
@@ -0,0 +1,21 @@
+local Players = game:GetService("Players")
+
+local client = Players.LocalPlayer
+local character = client.Character or client.CharacterAdded:Wait()
+
+local _cached = {}
+local _env = getsenv(character:WaitForChild("Saude").ACS_Client)
+
+for _, key in pairs(_env) do 
+    if typeof(key) == "function" then
+        _cached[#_cached + 1] = key
+    end
+end
+
+local engine do 
+    function engine:sprint()
+        _cached.Sprint()
+    end
+end
+
+return engine
